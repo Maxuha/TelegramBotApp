fdae6559bb345c9743a7d98928ad7cce7bab15b3
Maxuha <zykovmaksym@gmail.com>
2020-03-04 10:35:36 +0200

    Add parse mode id html

--- src/main/java/life/good/goodlife/controller/MonoBankController.java
+++ src/main/java/life/good/goodlife/controller/MonoBankController.java
@@ -2,6 +2,7 @@ package life.good.goodlife.controller;
 
 import com.github.telegram.mvc.api.BotController;
 import com.github.telegram.mvc.api.BotRequest;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 import life.good.goodlife.component.UserHistoryComponent;
@@ -25,6 +26,6 @@ public class MonoBankController {
     BaseRequest getCurrency(Long chatId) {
         userHistoryComponent.createUserHistory(userService.findByChatId(chatId).getId(), "/currency");
         String msg = currencyService.currency();
-        return new SendMessage(chatId, msg);
+        return new SendMessage(chatId, msg).parseMode(ParseMode.HTML).disableWebPagePreview(true);
     }
 }
