68bdc480117de085465b5af2eeaec6c6232e87d0
Maxuha <zykovmaksym@gmail.com>
2020-03-06 19:38:10 +0200

    Add method update location

--- src/main/java/life/good/goodlife/repos/map/LocationRepository.java
+++ src/main/java/life/good/goodlife/repos/map/LocationRepository.java
@@ -2,7 +2,11 @@ package life.good.goodlife.repos.map;
 
 import life.good.goodlife.model.map.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Location getByUserId(Long userId);
+
+    @Query(value = "UPDATE user_location SET latitude = ?1, longitude = ?2 WHERE id = ?3;", nativeQuery = true)
+    Location updateLocation(float lat, float lng, Long id);
 }
