072f8545f6acf4ac51d7a2ec1cd544b94ea426e8
Maxuha <zykovmaksym@gmail.com>
2020-03-06 20:44:00 +0200

    debug

--- src/main/java/life/good/goodlife/repos/map/LocationRepository.java
+++ src/main/java/life/good/goodlife/repos/map/LocationRepository.java
@@ -2,11 +2,13 @@ package life.good.goodlife.repos.map;
 
 import life.good.goodlife.model.map.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Location getByUserId(Long userId);
 
-    @Query(value = "UPDATE user_location SET latitude = ?1, longitude = ?2 WHERE id = ?3;", nativeQuery = true)
+    @Modifying
+    @Query(value = "update user_location set latitude = ?1, longitude = ?2 where id = ?3;", nativeQuery = true)
     void updateLocation(Float lat, Float lng, Long id);
 }
