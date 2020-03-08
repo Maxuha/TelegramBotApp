a15a5cb7bdec7e5b52ef191061f20c2bec069861
Maxuha <zykovmaksym@gmail.com>
2020-03-08 18:38:21 +0200

    Add transient

--- src/main/java/life/good/goodlife/model/monobonk/Statement.java
+++ src/main/java/life/good/goodlife/model/monobonk/Statement.java
@@ -1,9 +1,6 @@
 package life.good.goodlife.model.monobonk;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "monobank_statement")
@@ -44,6 +41,7 @@ public class Statement {
     @Column
     private Boolean hold;
 
+    @Transient
     private Balance balanceCount;
 
     public String getId() {
