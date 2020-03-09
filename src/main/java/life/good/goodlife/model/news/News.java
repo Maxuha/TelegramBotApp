4eb90fb533c7c957511bb138d5c45b5ca04c4f19
Maxuha <zykovmaksym@gmail.com>
2020-03-09 09:17:13 +0200

    Add model news

---
+++ src/main/java/life/good/goodlife/model/news/News.java
@@ -0,0 +1,31 @@
+package life.good.goodlife.model.news;
+
+public class News {
+    private String status;
+    private Integer totalResults;
+    private Article[] articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Article[] getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Article[] articles) {
+        this.articles = articles;
+    }
+}
