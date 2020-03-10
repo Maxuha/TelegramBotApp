c986250c36442ebb84ea31e49929ef44b0679b50
Maxuha <zykovmaksym@gmail.com>
2020-03-10 09:32:56 +0200

    Add exception to main class

--- src/main/java/life/good/goodlife/GoodLifeApplication.java
+++ src/main/java/life/good/goodlife/GoodLifeApplication.java
@@ -2,6 +2,8 @@ package life.good.goodlife;
 
 import com.github.telegram.mvc.api.EnableTelegram;
 import com.github.telegram.mvc.config.TelegramBotBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +13,18 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 @EnableTelegram
 public class GoodLifeApplication implements TelegramMvcConfiguration {
+    private static Logger logger = LoggerFactory.getLogger(GoodLifeApplication.class);
+
     @Autowired
     private Environment environment;
 
     public static void main(String[] args) {
-        SpringApplication.run(GoodLifeApplication.class, args);
+        try {
+            SpringApplication.run(GoodLifeApplication.class, args);
+        } catch (Exception e) {
+            logger.error("Run application problem: " + e.getMessage());
+        }
+
     }
 
     @Override
