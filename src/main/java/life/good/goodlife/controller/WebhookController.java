433cf8329240c38c58796bbe7d3873c66d2acc1f
Maxuha <zykovmaksym@gmail.com>
2020-03-11 12:23:38 +0200

    .

--- src/main/java/life/good/goodlife/controller/WebhookController.java
+++ src/main/java/life/good/goodlife/controller/WebhookController.java
@@ -56,7 +56,7 @@ public class WebhookController {
             data = writeOff(webhook);
         }
         System.out.println("ac: " + webhook.getData().getAccount());
-        String clientId = loginService.getAccountById(webhook.getData().getAccount()).getId();
+        String clientId = loginService.getAccountById(webhook.getData().getAccount()).getClientId();
         SendMessage sendMessage = new SendMessage(loginService.getUserIdByClientId(clientId), data).disableNotification(true)
                 .disableWebPagePreview(true).parseMode(ParseMode.HTML);
         telegramBotExecuteComponent.sendMessage(sendMessage);
