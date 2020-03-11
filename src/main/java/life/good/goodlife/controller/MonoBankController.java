03e3e9bb2ae2b6617b7618df55f4e1b4c6f9ddc0
Maxuha <zykovmaksym@gmail.com>
2020-03-11 22:37:32 +0200

    .

--- src/main/java/life/good/goodlife/controller/MonoBankController.java
+++ src/main/java/life/good/goodlife/controller/MonoBankController.java
@@ -148,7 +148,7 @@ public class MonoBankController {
         int index = 0;
         for (Account account : accounts) {
             for (int i = 0; i < account.getMaskedPan().length; i++) {
-                accountButtons[index][0] = "Карта\n" + account.getType() + "\n" + account.getCurrencyCode() + "\n" + account.getMaskedPan()[i];
+                accountButtons[index][0] = "Карта, " + account.getType() + ", " + account.getCurrencyCode() + "\n" + account.getMaskedPan()[i];
                 index++;
             }
         }
