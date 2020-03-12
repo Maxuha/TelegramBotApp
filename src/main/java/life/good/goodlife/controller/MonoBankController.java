bce04cc697ec184c976690a650fdab3e3a6ddcfd
Maxuha <zykovmaksym@gmail.com>
2020-03-12 13:42:11 +0200

    .

--- src/main/java/life/good/goodlife/controller/MonoBankController.java
+++ src/main/java/life/good/goodlife/controller/MonoBankController.java
@@ -168,7 +168,7 @@ public class MonoBankController {
         }
         accountButtons[accounts.size()][0] = Buttons.mainButton[1];
         Keyboard replyKeyboardMarkup = new ReplyKeyboardMarkup(accountButtons).resizeKeyboard(true);
-        SendMessage sendMessage = new SendMessage(chatId, "Выбери карту").parseMode(ParseMode.HTML);
+        SendMessage sendMessage = new SendMessage(chatId, "Выбери карту").parseMode(ParseMode.HTML).disableWebPagePreview(true);
         sendMessage.replyMarkup(replyKeyboardMarkup);
         return sendMessage;
     }
