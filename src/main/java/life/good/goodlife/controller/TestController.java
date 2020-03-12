56f6c6afa72a457da317c26472320a65aec04331
Maxuha <zykovmaksym@gmail.com>
2020-03-12 19:01:30 +0200

    .

--- src/main/java/life/good/goodlife/controller/TestController.java
+++ src/main/java/life/good/goodlife/controller/TestController.java
@@ -1,6 +1,10 @@
 package life.good.goodlife.controller;
 
 import life.good.goodlife.statics.Request;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,8 +29,13 @@ public class TestController {
     }
 
     @RequestMapping(path = "/test", method = RequestMethod.GET)
-    public ResponseEntity <?> test3() {
-        String result = Request.get("https://jump-to-infinity.com/index5.php");
+    public ResponseEntity <?> test3() throws IOException {
+        Document doc = Jsoup.connect("https://jump-to-infinity.com/index5.php")
+                .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                .referrer("http://www.google.com")
+                .get();
+        Elements elements = doc.select("a");
+        String result = elements.text();
         return ResponseEntity.ok(result);
     }
 }
