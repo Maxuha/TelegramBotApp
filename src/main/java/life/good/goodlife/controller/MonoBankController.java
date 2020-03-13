61eea126094d545df4d1ef8bba481245a26bafc7
Maxuha <zykovmaksym@gmail.com>
2020-03-13 21:35:08 +0200

    .

--- src/main/java/life/good/goodlife/controller/MonoBankController.java
+++ src/main/java/life/good/goodlife/controller/MonoBankController.java
@@ -194,6 +194,15 @@ public class MonoBankController {
             logger.error("Incorrectly url - " + e.getMessage());
         }
         WebRequest requestSettings = new WebRequest(url, HttpMethod.GET);
+        requestSettings.setAdditionalHeader("Accept", "*/*");
+        requestSettings.setAdditionalHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        requestSettings.setAdditionalHeader("Referer", "REFURLHERE");
+        requestSettings.setAdditionalHeader("Accept-Language", "en-US,en;q=0.8");
+        requestSettings.setAdditionalHeader("Accept-Encoding", "gzip,deflate,sdch");
+        requestSettings.setAdditionalHeader("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+        requestSettings.setAdditionalHeader("X-Requested-With", "XMLHttpRequest");
+        requestSettings.setAdditionalHeader("Cache-Control", "no-cache");
+        requestSettings.setAdditionalHeader("Pragma", "no-cache");
         //Request.get("http://jump-to-infinity.com/index5.php?cart=" + result.toString().trim());
         /*String result = "<b>Мой баланс: </b>\n\n" + "Карта: " + cart + "\n" +
                 "Тип: " + account.getType() + "\n" +
