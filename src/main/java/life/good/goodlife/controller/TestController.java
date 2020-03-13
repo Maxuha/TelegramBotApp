8f67143db9bc4fbd12e55fbe89adec896187b3af
Maxuha <zykovmaksym@gmail.com>
2020-03-13 10:01:42 +0200

    .

--- src/main/java/life/good/goodlife/controller/TestController.java
+++ src/main/java/life/good/goodlife/controller/TestController.java
@@ -45,10 +45,11 @@ public class TestController {
     public ResponseEntity <?> test3(@RequestBody String link) throws Exception {
         StringBuilder linkBuffer = new StringBuilder(link);
         linkBuffer.delete(0, 5);
+        byte[] linkBytes = Base64.decodeBase64(linkBuffer.toString().getBytes());
         //byte[] linkBytes = Base64.decodeBase64(linkBuffer.toString().getBytes());
-        //link = new String(linkBytes);
-        System.out.println("link: " + linkBuffer.toString());
-        URL url = new URL(linkBuffer.toString());
+        link = new String(linkBytes);
+        System.out.println("link: " + link);
+        URL url = new URL(link);
         BufferedImage img = ImageIO.read(url);
         File file = new File("F:\\downloaded.jpg");
         ImageIO.write(img, "jpg", file);
