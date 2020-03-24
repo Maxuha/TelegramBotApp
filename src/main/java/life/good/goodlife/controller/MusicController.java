affe96bbee183aaec2d452f04973ff7551674f12
Maxuha <zykovmaksym@gmail.com>
2020-03-24 17:19:13 +0200

    .

--- src/main/java/life/good/goodlife/controller/MusicController.java
+++ src/main/java/life/good/goodlife/controller/MusicController.java
@@ -14,8 +14,8 @@ public class MusicController {
     @BotRequest("Музыка")
     BaseRequest music(Long chatId) {
         Map<String, String> params = new HashMap<>();
-        params.put("api_key", System.getenv().get("music_api_key"));
-        String response = Request.doGet("http://www.last.fm/api/auth/", params, null);
-        return new SendMessage(chatId, response);
+        //params.put("api_key", System.getenv().get("music_api_key"));
+        //String response = Request.doGet("http://www.last.fm/api/auth/", params, null);
+        return new SendMessage(chatId, "response");
     }
 }
